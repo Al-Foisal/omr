a3c5f34c188e2c062b55055d8bf631f85ad0cc6c
Md Hafiz Al Foisal <alfoisal940700@gmail.com>
2023-07-26 17:30:59 +0600

    exam result table upload

---
+++ database/migrations/2023_07_26_165612_create_exam_results_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('exam_results', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('exam_id');
+            $table->unsignedBigInteger('subject_id');
+            $table->longText('answer');
+            $table->string('input_image')->nullable();
+            $table->string('output_image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('exam_results');
+    }
+};
