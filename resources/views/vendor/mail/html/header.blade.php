427064ba8a291f12046e9416f01cf3dec0947b6c
Md Hafiz Al Foisal <alfoisal940700@gmail.com>
2023-08-23 13:04:48 +0600

    mail system updated

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 
-<img src="{{ $url.'/'. $company->logo }}" class="logo" alt="Laravel Logo">
+<img src="{{ $url.$company->logo }}" class="logo" alt="Laravel Logo">
 
 </a>
 </td>
